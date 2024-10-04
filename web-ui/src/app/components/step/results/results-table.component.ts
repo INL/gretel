9d229dea4ac086528872b79e5bb3ae905d9b9732
KCMertens <koen.mertens@ivdnt.org>
2024-10-04 16:12:32 +0300

    Update url to most recent version of corpus

--- web-ui/src/app/components/step/results/results-table.component.ts
+++ web-ui/src/app/components/step/results/results-table.component.ts
@@ -163,7 +163,6 @@ export class ResultsTableComponent implements OnInit, OnDestroy, OnTypedChanges<
                 return this.columns.some(col => col.field === c.field);
             })
         
-            console.log('new hits in table', changes.filteredResults.currentValue);
             this.processedHits = changes.filteredResults.currentValue.map(hit => ({
                 ...hit.metaValues,
                 fileId: hit.fileId,
@@ -172,7 +171,7 @@ export class ResultsTableComponent implements OnInit, OnDestroy, OnTypedChanges<
                 highlightedSentence2: hit.highlightedSentence2,
                 previousSentence: hit.previousSentence,
                 nextSentence: hit.nextSentence,
-                blacklabUrl: `https://gcnd.ato.ivdnt.org/corpus-frontend/GCND_mei/docs/${hit.metaValues.docpid}/?query=${encodeURIComponent(`<s id="${hit.metaValues.sentence_id}"/>`)}`
+                blacklabUrl: `https://gcnd.ato.ivdnt.org/corpus-frontend/GCND-Hackathon/docs/${hit.metaValues.docpid}/?query=${encodeURIComponent(`<s id="${hit.metaValues.sentence_id}"/>`)}`
             }));
         }
     }
