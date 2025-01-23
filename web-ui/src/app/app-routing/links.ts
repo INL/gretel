62e68721748693d7cbc583c2976a8000def47e03
KCMertens <koen.mertens@ivdnt.org>
2025-01-23 12:20:00 +0300

    Allow links in navbar to point to external sources

--- web-ui/src/app/app-routing/links.ts
+++ web-ui/src/app/app-routing/links.ts
@@ -4,44 +4,37 @@
 interface Link {
     name: string;
     link: string;
-    proxy: boolean;
+    external?: boolean
 }
 
 const links: Link[] = [
     {
         name: 'Home',
         link: '/home',
-        proxy: false,
     },
     {
         name: 'Example-based Search',
         link: '/example-based-search',
-        proxy: true,
     },
     {
         name: 'XPath Search',
         link: '/xpath-search',
-        proxy: false,
     },
     {
         name: 'Multiword Expressions',
         link: '/mwe-search',
-        proxy: false,
     },
     {
         name: 'Upload',
         link: '/upload-treebank',
-        proxy: false,
     },
     {
         name: 'About',
         link: '/about',
-        proxy: false,
     },
     {
         name: 'Documentation',
         link: '/documentation',
-        proxy: false,
     },
 ];
 
